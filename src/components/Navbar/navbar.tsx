48a6c1b7c2208b3593cafb9ccf3c88316099158e
TrinhLe08 <khanhtrinhmc2021@gmail.com>
2023-07-02 16:14:03 +0700

    up-navbar-fe

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,36 +1,37 @@
-import { BiCreditCard } from "react-icons/bi";
-import { FiFileText } from "react-icons/fi";
-import { FiSettings } from "react-icons/fi";
-import { FiSun } from "react-icons/fi";
+import { AppWindow } from "lucide-react";
+import { FileText } from "lucide-react";
+import { Settings } from "lucide-react";
+import { Sun } from "lucide-react";
 
 export default function Navbar() {
     const style = {
-        span: "ml-3 font-medium text-sm pb-1",
-        iconMenu: "text-lg",
         menu: "flex h-8 w-40 p-2 rounded-md hover:bg-slate-100 cursor-pointer",
-        aFooter: "font-medium underline h-12 pb-4",
+        footerMenuSpan: "ml-3 font-medium text-sm pb-1",
+        footerLink: "font-medium underline h-12 pb-4",
     };
 
     return (
         <div className="Navbar grid h-full h-screen ">
+
             <div className="nav grid grid-cols-1 gap-y-8 h-16 mt-20 ml-24 h-[72vh] ">
                 <div className="menu grid grid-cols-1 gap-y-3 h-16 mt-1 ml-4 h-12">
                     <div className={style.menu}>
-                        <FiFileText className={style.iconMenu} />
-                        <span className={style.span}>Post</span>
+                        <FileText size={18} />
+                        <span className={style.footerMenuSpan}>Post</span>
                     </div>
                     <div className={style.menu}>
-                        <BiCreditCard className={style.iconMenu} />
-                        <span className={style.span}>Billing</span>
+                        <AppWindow size={18} />
+                        <span className={style.footerMenuSpan}>Billing</span>
                     </div>
                     <div className={style.menu}>
-                        <FiSettings className={style.iconMenu} />
-                        <span className={style.span}>Setting</span>
+                        <Settings size={18} />
+                        <span className={style.footerMenuSpan}>Setting</span>
                     </div>
                 </div>
             </div>
 
             <div className="footer flex items-end w-screen justify-between font border-t h-16">
+                
                 <div className="icon flex items-center text-xl mr-20  ml-12">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -48,31 +49,20 @@ export default function Navbar() {
                         <path d="M18 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3H6a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3V6a3 3 0 0 0-3-3 3 3 0 0 0-3 3 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 3 3 0 0 0-3-3z"></path>
                     </svg>
 
-                    <span className="text-sm mb-2 ">
-                        Built by
-                        <a href="" className={style.aFooter}>
-                            {" "}
-                            shadcn.{" "}
-                        </a>
-                        Hosted on
-                        <a href="" className={style.aFooter}>
-                            {" "}
-                            Vercel.{" "}
-                        </a>
-                        Illustrations by
-                        <a href="" className={style.aFooter}>
-                            {" "}
-                            Popsy.{" "}
-                        </a>
-                        The source code is available on
-                        <a href="" className={style.aFooter}>
-                            {" "}
-                            GitHub.{" "}
-                        </a>
-                    </span>
+                    <span className = 'text-sm mb-2 '>
+                         Built by
+                          <a href="" className={style.footerLink}> shadcn. </a> 
+                         Hosted on
+                          <a href="" className={style.footerLink}> Vercel. </a>
+                         Illustrations by
+                          <a href="" className={style.footerLink}> Popsy. </a> 
+                         The source code is available on
+                         <a href="" className={style.footerLink}> GitHub. </a>
+                     </span>
                 </div>
 
-                <FiSun className="text-xl mr-20  w-8 h-8 rounded-md hover:bg-slate-100 p-1.5 " />
+                <Sun className="text-xl mr-20  w-8 h-8 rounded-md hover:bg-slate-100 p-1.5 " />
+
             </div>
         </div>
     );
