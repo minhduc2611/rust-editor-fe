f26d32f6d750c031e567bd632459c8975858d9c9
TrinhLe08 <khanhtrinhmc2021@gmail.com>
2023-07-02 16:33:45 +0700

    up my navbar

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -12,7 +12,6 @@ export default function Navbar() {
 
     return (
         <div className="Navbar grid h-full h-screen ">
-
             <div className="nav grid grid-cols-1 gap-y-8 h-16 mt-20 ml-24 h-[72vh] ">
                 <div className="menu grid grid-cols-1 gap-y-3 h-16 mt-1 ml-4 h-12">
                     <div className={style.menu}>
@@ -31,7 +30,6 @@ export default function Navbar() {
             </div>
 
             <div className="footer flex items-end w-screen justify-between font border-t h-16">
-                
                 <div className="icon flex items-center text-xl mr-20  ml-12">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -48,21 +46,19 @@ export default function Navbar() {
                         <path d="M18 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3H6a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3V6a3 3 0 0 0-3-3 3 3 0 0 0-3 3 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 3 3 0 0 0-3-3z"></path>
                         <path d="M18 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3H6a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3V6a3 3 0 0 0-3-3 3 3 0 0 0-3 3 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 3 3 0 0 0-3-3z"></path>
                     </svg>
-
-                    <span className = 'text-sm mb-2 '>
-                         Built by
-                          <a href="" className={style.footerLink}> shadcn. </a> 
-                         Hosted on
-                          <a href="" className={style.footerLink}> Vercel. </a>
-                         Illustrations by
-                          <a href="" className={style.footerLink}> Popsy. </a> 
-                         The source code is available on
-                         <a href="" className={style.footerLink}> GitHub. </a>
+                    <span className = {style.footerLink}>
+                        Built by
+                         <a href="" className={style.footerLink}> shadcn. </a> 
+                        Hosted on
+                         <a href="" className={style.footerLink}> Vercel. </a>
+                        Illustrations by
+                         <a href="" className={style.footerLink}> Popsy. </a> 
+                        The source code is available on
+                        <a href="" className={style.footerLink}> GitHub. </a>
                      </span>
                 </div>
 
-                <Sun className="text-xl mr-20  w-8 h-8 rounded-md hover:bg-slate-100 p-1.5 " />
-
+                <Sun className="text-xl mr-20 w-8 h-8 rounded-md hover:bg-slate-100 p-1.5 " />
             </div>
         </div>
     );
